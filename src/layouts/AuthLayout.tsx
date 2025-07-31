5cddbf5b403ea7d94ede8ffc72a0bf652d916d4d
habibiefattahillah <habibiefattahillah78@gmail.com>
2025-07-31 22:14:40 +0700

    ssh

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -6,7 +6,7 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
       <div className="w-full md:w-1/2 flex flex-col justify-center items-center px-6 md:px-20">
         <div className="w-full max-w-sm space-y-4">
           <div className="flex justify-center gap-3">
-            <img src="/public/Logo.png" alt="logo" className="mb-4" />
+            <img src="/Logo.png" alt="logo" className="mb-4" />
             <h1 className="text-xl font-semibold">SIMS PPOB</h1>
           </div>
           {children}
@@ -15,7 +15,7 @@ export default function AuthLayout({ children }: { children: ReactNode }) {
 
       <div className="hidden md:block w-1/2 bg-[#FFF2F2] relative">
         <img
-          src="/public/Illustrasi Login.png"
+          src="/Illustrasi Login.png"
           alt="illustration"
           className="w-full h-full object-cover"
         />
